8dc7fceff2609e32c2ee36ea50c54a8ea790bf50
A-GHOSH-dev <75447439+A-GHOSH-dev@users.noreply.github.com>
2021-06-18 20:55:50 +0530

    Add files via upload

--- index10.js
+++ index10.js
@@ -59,3 +59,30 @@ function print_city(city_id, city_index){
 		option_str.options[option_str.length] = new Option(city_arr[i],city_arr[i]);
 	}
 }
+
+/*
+This script is identical to the above JavaScript function.
+*/
+var ct = 1;
+function new_link()
+{
+	ct++;
+	var div1 = document.createElement('div');
+	div1.id = ct;
+	// link to delete extended form elements
+	var delLink = '<div style="text-align:right;margin-right:65px"><a href="javascript:delIt('+ ct +')">Del</a></div>';
+	div1.innerHTML = document.getElementById('newlinktpl').innerHTML + delLink;
+	document.getElementById('newlink').appendChild(div1);
+}
+// function to delete the newly added set of elements
+function delIt(eleId)
+{
+	d = document;
+	var ele = d.getElementById(eleId);
+	var parentEle = d.getElementById('newlink');
+	parentEle.removeChild(ele);
+}
+
+
+
+
